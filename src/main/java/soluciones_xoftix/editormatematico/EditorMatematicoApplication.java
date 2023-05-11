f58d8010e7b2c89d00906347203acb4e7dfb39b0
joburbanop <joburbanop589@gmail.com>
2023-05-11 14:28:51 -0500

    frontend 80%

---
+++ src/main/java/soluciones_xoftix/editormatematico/EditorMatematicoApplication.java
@@ -0,0 +1,13 @@
+package soluciones_xoftix.editormatematico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorMatematicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditorMatematicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soluciones_xoftix/editormatematico/EditorMatematicoApplicationTests.java
@@ -0,0 +1,13 @@
+package soluciones_xoftix.editormatematico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorMatematicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
